fecc451582fa3dc0b19a274031d7d886c4c6c4db
SBENTCODES <147941416+SBENTCODES@users.noreply.github.com>
2024-05-31 17:41:35 +0530

    login page courasel

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Navbar from "./Components/Navbar";
+import Slider from "./Components/Slider";
+import Login from "./Pages/Login";
+
+function App() {
+
+
+
+  
+  return (
+    <div >
+      <Navbar/>
+      <Login/>
+
+      {/* <Slider/> */}
+
+    
+  
+    </div>
+  );
+}
+
+export default App;
